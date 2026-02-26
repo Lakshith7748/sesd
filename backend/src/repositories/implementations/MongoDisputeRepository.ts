54042d1c88ad193228d3ecbe25a8f3f02efdeafe
Lakshith <lakshith.achuta@gmail.com>
2026-02-26 04:30:00 +0000

    chore: dependencies update

---
+++ backend/src/repositories/implementations/MongoDisputeRepository.ts
@@ -0,0 +1,29 @@
+import DisputeModel, { IDispute } from '../../models/Dispute';
+import { IDisputeRepository } from '../interfaces/IDisputeRepository';
+
+export class MongoDisputeRepository implements IDisputeRepository {
+  async findById(id: string): Promise<IDispute | null> {
+    return DisputeModel.findById(id).exec();
+  }
+
+  async findByProject(projectId: string): Promise<IDispute | null> {
+    return DisputeModel.findOne({ projectId }).exec();
+  }
+
+  async save(data: Partial<IDispute>): Promise<IDispute> {
+    const dispute = new DisputeModel(data);
+    return dispute.save();
+  }
+
+  async update(id: string, data: Partial<IDispute>): Promise<IDispute | null> {
+    return DisputeModel.findByIdAndUpdate(id, data, { new: true }).exec();
+  }
+
+  async findAll(): Promise<IDispute[]> {
+    return DisputeModel.find()
+      .populate('projectId', 'title')
+      .populate('raisedBy', 'name email')
+      .sort({ createdAt: -1 })
+      .exec();
+  }
+}
