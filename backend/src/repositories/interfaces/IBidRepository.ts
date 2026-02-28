d61be5960a9fa87605d19b03e82541f442d811e6
Lakshith <lakshith.achuta@gmail.com>
2026-02-28 09:30:00 +0000

    style: typography update

---
+++ backend/src/repositories/interfaces/IBidRepository.ts
@@ -0,0 +1,11 @@
+import { ClientSession } from 'mongoose';
+import { IBid } from '../../models/Bid';
+
+export interface IBidRepository {
+  findById(id: string): Promise<IBid | null>;
+  findByProject(projectId: string): Promise<IBid[]>;
+  findByFreelancerAndProject(freelancerId: string, projectId: string): Promise<IBid | null>;
+  findByFreelancer(freelancerId: string): Promise<IBid[]>;
+  save(data: Partial<IBid>): Promise<IBid>;
+  update(id: string, data: Partial<IBid>, session?: ClientSession): Promise<IBid | null>;
+}
